49207723b9dc6320d92af8dc2056024cabe55794
Mavuyisi-Chabane <2586216@students.wits.ac.za>
2026-03-02 16:08:36 +0200

    feat: add JavaScript functionality to Country Explorer app

---
+++ script.js
@@ -0,0 +1,80 @@
+const countryInput = document.getElementById("country-input");
+const searchBtn = document.getElementById("search-btn");
+const countryInfo = document.getElementById("country-info");
+const borderingCountries = document.getElementById("bordering-countries");
+const loadingSpinner = document.getElementById("loading-spinner");
+const errorMessage = document.getElementById("error-message");
+
+async function searchCountry(countryName){
+    try{
+        if(!countryName){
+            throw new Error("Enter a country name.");
+        }
+
+        loadingSpinner.classList.remove('hidden');
+
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+
+        if(!response.ok){
+            throw new Error("Country not found.");
+        }
+
+        const data = await response.json();
+        const country = data[0];
+
+        countryInfo.innerHTML = `
+        <h2>${country.name.common}</h2>
+        <p><strong>Capital:</strong> ${country.capital[0]}</p>
+        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+        <p><strong>Region:</strong> ${country.region}</p>
+        <img src="${country.flags.svg}" alt="${country.name.common} flag">
+        `;
+
+        countryInfo.classList.remove('hidden');
+
+        if(country.borders){
+            for(let code of country.borders){
+                const borderResponse = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+                const borderData = await borderResponse.json();
+                const borderCountry = borderData[0];
+
+                borderingCountries.innerHTML += `
+                    <div class="border-card">
+                        <h4>${borderCountry.name.common}</h4>
+                        <img src="${borderCountry.flags.svg}" alt="${borderCountry.name.common} flag">
+                    <div>
+                `;
+            }
+
+            borderingCountries.classList.remove('hidden');
+        }
+
+    }
+
+    catch (error){
+        errorMessage.textContent = `Error: ${error.message}`;
+        errorMessage.style.display = 'block';
+
+    }
+
+    finally{
+        loadingSpinner.style.display = 'none';
+
+    }
+}
+
+searchBtn.addEventListener('click', () => {
+    const country = countryInput.value.trim();
+    if(country){
+        searchCountry(country);
+    }
+});
+
+countryInput.addEventListener('keypress', (e) => {
+    if(e.key === "Enter"){
+        const country = countryInput.value.trim();
+        if(country){
+            searchCountry(country);
+        }
+    }
+});
